bcc0f111f74e447ffd3db0f599fe00f00001b5a5
Rajeshwarikota <kotarajeswari003.com>
2023-02-14 12:43:17 +0530

    [Rajeshwari] Adding Stacks and Queues

---
+++ StackAndQue/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("...Stacks and queues");
+            Console.ReadLine();
+        }
+    }
+}
